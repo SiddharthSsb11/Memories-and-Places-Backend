d059340886aeb77c0372d87ebb92333ffde1ec82
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2021-10-25 23:53:03 +0530

    BUG Fixed

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -33,20 +33,20 @@ const getPlaceById = (req, res, next) => {
   res.json({ place }); //{ place: place }
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
-  const place = DUMMY_PLACES.find((p) => {
+  const places = DUMMY_PLACES.filter((p) => {
     return p.creator === userId;
   });
 
-  if (!place) {
+  if (!places || places.length === 0) {//there can be multiple places shared/posted by a single creator/user
     return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
+      new HttpError("Could not find any places for the provided user id.", 404)
     );
   }
 
-  res.json({ place });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -91,7 +91,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
