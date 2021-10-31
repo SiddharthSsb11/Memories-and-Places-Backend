0d68bfc72428cd1d96757b3db61ad55c90536918
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2021-10-31 23:51:29 +0530

    User Schema Added

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    image: { type: String, required: true },
+    places: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator);
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
