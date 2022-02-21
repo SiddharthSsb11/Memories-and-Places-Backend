4f559c2ca554eb16f89e0cd2d4bd79cacc2ceb83
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-02-21 23:47:09 +0530

    comment feature field added in places schema

--- models/place.js
+++ models/place.js
@@ -1,20 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const placeSchema = new Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    image: { type: String, required: true },
-    address: { type: String, required: true },
-    creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User'},
-    likes: [
-        {
-          user: { type: Schema.Types.ObjectId, ref: 'users'}
-        }
-    ]
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  address: { type: String, required: true },
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  likes: [
+    {
+      user: { type: Schema.Types.ObjectId, ref: "users" },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const Place = mongoose.model('Place', placeSchema);
+const Place = mongoose.model("Place", placeSchema);
 
 module.exports = Place;
